999df032af3401cdf19c6de23733e809ce32ee48
Kurt Choi <choi.kurt3@gmail.com>
2023-11-12 00:36:57 -0500

    Add rough notation animation to hero page

--- src/app/components/navigation/navbar/Logo.js
+++ src/app/components/navigation/navbar/Logo.js
@@ -3,7 +3,7 @@ const { default: Link } = require("next/link")
 const Logo = () => {
     return ( 
         <Link href="/">
-            <div className="text-xl font-bold select-none hover:text-slate-600 transition duration-75 ease-in-out tracking-tighter text-slate-200">
+            <div className="text-xl font-bold select-none transition duration-75 ease-in-out tracking-tighter text-slate-200 gradient-underline">
                 Kurt Choi
             </div>
         </Link>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-'use client'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAmazon } from '@fortawesome/free-brands-svg-icons'
-
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function Home() {
   return (
     <div className="mt-4 text-white">
       <div className="flex-col space-y-3 py-8 text-3xl font-semibold leading-tight pr-2">
-        <div className="">Hello, my name is <p className="text-gradient-primary font-bold">Kurt</p>.</div>
-        <div>I'm currently a Software Engineer at <div className="inline-block gradient-underline"> Amazon</div>.</div>
+        <RoughNotationGroup show={true}>
+          <div className="">Hello, my name is <p className="text-gradient-primary font-bold">Kurt</p>.</div>
+          <div>I'm currently a <RoughNotation type="highlight" color="#3b82f6">Software Engineer</RoughNotation> at <RoughNotation type="underline" color="#3b82f6">Amazon</RoughNotation>.</div>
+        </RoughNotationGroup>
       </div>
       <div>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
