b45772b03052e4d3d94e0cde356f47bc6579e1a0
Kurt Choi <choi.kurt3@gmail.com>
2023-11-11 23:21:12 -0500

    Update landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div className="mt-4 text-white">
-      Hello
+      <div className="flex-col space-y-3 py-8 text-3xl font-semibold leading-tight">
+        <p className="">Hello, my name is <p className="bg-gradient-to-br from-cyan-400 via-blue-500 to-purple-500 inline-block text-transparent bg-clip-text font-bold">Kurt</p>.</p>
+        <p>I'm currently a Software Engineer at Amazon.</p>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
     </div>
   )
 }
