534afdcaa01bf8f14132b7852d89610fdddb9416
Kurt Choi <choi.kurt3@gmail.com>
2023-11-11 23:20:48 -0500

    Layout tweak

--- src/app/components/navigation/navbar/Logo.js
+++ src/app/components/navigation/navbar/Logo.js
@@ -3,8 +3,8 @@ const { default: Link } = require("next/link")
 const Logo = () => {
     return ( 
         <Link href="/">
-            <div className="text-white text-lg font-bold select-none hover:text-slate-600 transition duration-75 ease-in-out">
-                Kurt
+            <div className="text-white text-xl font-bold select-none hover:text-slate-600 transition duration-75 ease-in-out">
+                Kurt Choi
             </div>
         </Link>
     );
--- src/app/components/navigation/navbar/Navbar.js
+++ src/app/components/navigation/navbar/Navbar.js
@@ -3,11 +3,11 @@ import Logo from "./Logo";
 
 const Navbar = () => {
     return ( 
-        <div className="h-14 bg-deepgray sticky top-0 select-none">
+        <div className="py-8 bg-deepgray sticky top-0 select-none">
             <div className="container mx-auto h-full px-4">
                 <div className="flex justify-between items-center h-full">
                     <Logo />
-                    <ul className="hidden md:flex gap-x-6 text-white text-sm">
+                    <ul className="hidden md:flex gap-x-6 text-white text-sm font-semibold">
                         <li>
                             <Link href="/about">
                                 <p className="hover:text-slate-600 transition duration-75 ease-in-out">About</p>
@@ -21,7 +21,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-            <div className="mx-auto border-b drop-shadow-[0_5px_3px_rgba(255,255,255,0.25)]"/>
+            {/* <div className="mx-auto border-b drop-shadow-[0_5px_3px_rgba(255,255,255,0.25)]"/> */}
         </div>
      );
 }
