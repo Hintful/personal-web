b7fbc4c998cd8b66a6719dc565e9328f56439e22
Kurt Choi <choi.kurt3@gmail.com>
2023-11-12 02:01:28 -0500

    Tweak logo

--- src/app/components/navigation/navbar/Logo.js
+++ src/app/components/navigation/navbar/Logo.js
@@ -3,8 +3,9 @@ const { default: Link } = require("next/link")
 const Logo = () => {
     return ( 
         <Link href="/">
-            <div className="text-xl font-bold select-none transition duration-75 ease-in-out tracking-tighter text-slate-200 gradient-underline">
-                Kurt Choi
+            <div className="text-xl font-bold select-none transition duration-75 ease-in-out tracking-tighter text-slate-200">
+                <span className="gradient-underline">Kurt Choi</span>
+                <span className="text-slate-500 text-base">.dev</span>
             </div>
         </Link>
     );
