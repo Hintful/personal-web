9c8a21e6998a7de5f5c1f14566e2e0242681b746
Kurt Choi <choi.kurt3@gmail.com>
2023-11-12 20:44:06 -0500

    Tweak HeroBody content

--- src/app/components/hero/HeroBody.js
+++ src/app/components/hero/HeroBody.js
@@ -1,7 +1,9 @@
 const HeroBody = () => {
     return (  
         <div className="text-lg text-white text-opacity-70">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p>
+                Currently working in a Backend focused team at Amazon, my professional experience is mostly from backend development. However, I'm also very interested in frontend development and often work on the frontend aspect in personal/group projects.
+            </p>
         </div>
     );
 }
