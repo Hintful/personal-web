831e5209107b56d173512f448b0a0ae8787ec827
Kurt Choi <choi.kurt3@gmail.com>
2023-11-12 22:46:43 -0500

    Add rough notation points to HeroBody

--- src/app/components/hero/HeroBody.js
+++ src/app/components/hero/HeroBody.js
@@ -1,15 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAmazon } from '@fortawesome/free-brands-svg-icons';
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const HeroBody = () => {
     return (  
         <div className="text-lg text-white text-opacity-70 space-y-2">
-            <p>
-                Currently working in a Backend focused team at <FontAwesomeIcon className="ml-0.5" icon={faAmazon} /> Amazon, my professional experience is mostly from backend development. 
-            </p>
-            <p>
-                However, I'm also very interested in frontend development and often work on the frontend aspect in personal/group projects.
-            </p>
+            <RoughNotationGroup show={true}>
+                <p>
+                    Currently <RoughNotation>working in a Backend focused team</RoughNotation> at <FontAwesomeIcon className="ml-0.5" icon={faAmazon} /> Amazon, my professional experience
+                     is mostly from backend development. 
+                </p>
+                <p>
+                    However, I'm also <RoughNotation type={"circle"}>very interested</RoughNotation> in <RoughNotation>frontend development</RoughNotation> and often work on the frontend aspect in personal/group projects.
+                </p>
+            </RoughNotationGroup>
         </div>
     );
 }
