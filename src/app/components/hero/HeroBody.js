b3936de1b16b589ec1d251657da9b97bdcc2cbee
Kurt Choi <choi.kurt3@gmail.com>
2023-11-12 21:29:18 -0500

    HeroBody minor tweak

--- src/app/components/hero/HeroBody.js
+++ src/app/components/hero/HeroBody.js
@@ -1,8 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAmazon } from '@fortawesome/free-brands-svg-icons';
+
 const HeroBody = () => {
     return (  
-        <div className="text-lg text-white text-opacity-70">
+        <div className="text-lg text-white text-opacity-70 space-y-2">
             <p>
-                Currently working in a Backend focused team at Amazon, my professional experience is mostly from backend development. However, I'm also very interested in frontend development and often work on the frontend aspect in personal/group projects.
+                Currently working in a Backend focused team at <FontAwesomeIcon className="ml-0.5" icon={faAmazon} /> Amazon, my professional experience is mostly from backend development. 
+            </p>
+            <p>
+                However, I'm also very interested in frontend development and often work on the frontend aspect in personal/group projects.
             </p>
         </div>
     );
