1a3b358a8b2f26e5458228a5bb4698d1c5e8a9d4
Kurt Choi <choi.kurt3@gmail.com>
2023-11-12 20:44:20 -0500

    Add ambient blurred backdrop touch

--- src/app/components/hero/HeroHeader.js
+++ src/app/components/hero/HeroHeader.js
@@ -2,11 +2,15 @@ import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const HeroHeader = () => {
     return ( 
-        <div className="flex-col space-y-3 pt-8 text-3xl font-semibold leading-tight pr-2 text-white">
-            <RoughNotationGroup show={true}>
-            <div className="">Hello, my name is <p className="text-gradient-primary font-bold">Kurt</p>.</div>
-            <div>I'm currently a <RoughNotation type="highlight" color="#3b82f6" multiline="true">Software Engineer</RoughNotation> at <RoughNotation type="underline" color="#3b82f6">Amazon</RoughNotation>.</div>
-            </RoughNotationGroup>
+        <div className="pt-8 text-3xl font-semibold leading-tight pr-2 text-white">
+            <div className="relative z-0 -mb-36 rounded-full blur-[128px] opacity-100 w-48 h-48 bg-blue-500 top-20 -left-10" />
+            <div className="relative z-0 -mb-36 rounded-full blur-[96px] opacity-75 w-36 h-48 bg-purple-300 top-20 left-40" />
+            <div className="relative z-10">
+                <RoughNotationGroup show={true}>
+                    <div className="z-10">Hello, my name is <p className="text-gradient-primary font-bold">Kurt</p>.</div>
+                    <div>I'm currently a <RoughNotation type="highlight" color="#3b82f6" multiline="true">Software Engineer</RoughNotation> at <RoughNotation type="underline" color="#3b82f6">Amazon</RoughNotation>.</div>
+                </RoughNotationGroup>
+            </div>
       </div>
     );
 }
