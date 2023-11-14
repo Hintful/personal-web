a87e90cab5db7e00b5b9bc423e5211ac70248689
Kurt Choi <choi.kurt3@gmail.com>
2023-11-13 21:57:06 -0500

    Add an item to HeroTimeline

--- src/app/components/hero/timeline/HeroTimeline.js
+++ src/app/components/hero/timeline/HeroTimeline.js
@@ -2,7 +2,7 @@ import HeroTimelineItem from "./HeroTimelineItem";
 import { RoughNotation } from "react-rough-notation";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAmazon } from '@fortawesome/free-brands-svg-icons';
-import { faGraduationCap, faTerminal, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faGraduationCap, faTerminal, faStar, faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
 
 const HeroTimeline = () => {
     const timelineItems = [
@@ -26,6 +26,12 @@ const HeroTimeline = () => {
             icon: <FontAwesomeIcon className="h-3.5 w-3.5" icon={faTerminal} />
         },
         {
+            main: "Immigrated to Canada",
+            time: "2007",
+            content: "Immigrated over to Canada with my family in 2007",
+            icon: <FontAwesomeIcon className="h-3.5 w-3.5" icon={faPlaneDeparture} />
+        },
+        {
             main: "Born in South Korea",
             time: "1995",
             content: "I was born in South Korea in 1995, and my interest in computers date all the way before Y2K in 2000 as I was fascinated by computers \
@@ -38,7 +44,7 @@ const HeroTimeline = () => {
             <div className="relative z-0 -mb-36 rounded-full blur-[160px] opacity-100 w-48 h-48 bg-blue-500 top-72 left-72" />
             <div className="relative z-0 -mb-36 rounded-full blur-[120px] opacity-75 w-36 h-48 bg-purple-300 top-96 left-48" />
             <div className="flex-col space-y-8 z-10">
-                <span className="text-xl font-bold"><RoughNotation color="#3b82f6" animationDelay="1500" show={true}>Timeline</RoughNotation></span>
+                <span className="text-2xl tracking-tight font-semibold"><RoughNotation color="#3b82f6" animationDelay="1500" show={true}>Timeline</RoughNotation></span>
                 <ol className="relative border-s border-gray-200 dark:border-gray-700 space-y-8">
                     { timelineItems.map((item, idx) => {
                         return <HeroTimelineItem main={item.main} time={item.time} content={item.content} icon={item.icon} index={idx} key={idx} />
