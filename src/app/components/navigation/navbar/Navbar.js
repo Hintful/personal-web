e9f1b60a64e82eb6a977b1469d8e3daba318b0e5
Kurt Choi <choi.kurt3@gmail.com>
2023-11-13 20:45:40 -0500

    Mappify navbar items

--- src/app/components/navigation/navbar/Navbar.js
+++ src/app/components/navigation/navbar/Navbar.js
@@ -2,26 +2,39 @@ import Link from "next/link";
 import Logo from "./Logo";
 
 const Navbar = () => {
+    const items = [
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Project",
+            link: "/project"
+        },
+        {
+            name: "Contact",
+            link: "/contact"
+        }
+    ]
+    
     return ( 
         <div className="py-8 sticky top-0 select-none">
             <div className="container mx-auto h-full px-4">
                 <div className="flex justify-between items-center h-full">
                     <Logo />
                     <ul className="hidden md:flex gap-x-6 text-slate-200 text-sm font-semibold">
-                        <li>
-                            <Link href="/about">
-                                <p className="hover:text-slate-600 transition duration-75 ease-in-out">About</p>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/contact">
-                                <p className="hover:text-slate-600 transition duration-75 ease-in-out">Contact</p>
-                            </Link>
-                        </li>
+                        {
+                            items.map(item => (
+                                <li>
+                                    <Link href={item.link}>
+                                        <p className="hover:text-slate-600 transition duration-75 ease-in-out">{item.name}</p>
+                                    </Link>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
             </div>
-            {/* <div className="mx-auto border-b drop-shadow-[0_5px_3px_rgba(255,255,255,0.25)]"/> */}
         </div>
      );
 }
