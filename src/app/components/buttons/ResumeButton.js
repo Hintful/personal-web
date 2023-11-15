aab54b5b221636c88822a3934c81a765355daae2
Kurt Choi <choi.kurt3@gmail.com>
2023-11-14 22:26:23 -0500

    Fix issue where resume button clickable area extends the button size

--- src/app/components/buttons/ResumeButton.js
+++ src/app/components/buttons/ResumeButton.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const ResumeButton = () => {
     return (
-        <div>
+        <div className="w-fit">
             <Link href="/cv.pdf" target="_blank">
                 <div className="flex relative rounded-full w-fit py-2 pl-4 pr-3 cursor-pointer items-center justify-center bg-blue-500 transition-all duration-150 ease-in-out hover:gradient-primary">
                     <div className="flex justify-items-center items-center opacity-100 space-x-1">
