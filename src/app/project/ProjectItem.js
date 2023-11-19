a89b41658fff4105388dd8a347ed32bbc6108108
Kurt Choi <choi.kurt3@gmail.com>
2023-11-18 21:02:38 -0500

    Complete project page/item/tag structure

--- src/app/project/ProjectItem.js
+++ src/app/project/ProjectItem.js
@@ -1,7 +1,32 @@
+import ProjectTag from "./ProjectTag";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import Link from "next/link";
+
 const ProjectItem = (props) => {
     return (  
-        <div className="flex-col border w-full rounded-lg">
-            <span>{ props.item.name }</span>
+        <div className="flex-col border w-full rounded-lg p-4 space-y-1">
+            <div className="flex items-center space-x-2 w-full justify-between">
+                <span className="text-3xl tracking-tight font-semibold">{ props.item.name }</span>
+                <div className="flex items-center space-x-1">
+                    <Link href={props.item.link} target="_blank">
+                        <FontAwesomeIcon className="hover:text-gray-700 transition ease-in-out" icon={faSquareGithub} size="2xl" />
+                    </Link>
+                </div>
+            </div>
+            <div className="flex space-x-1">
+                {
+                    props.item.tags.map(tag => (
+                        <ProjectTag name={tag} />
+                    ))
+                }
+            </div>
+            <div className="pt-4">
+                <span className="text-sm">
+                    { props.item.description }
+                </span>
+            </div>
+            
         </div>
     );
 }
--- src/app/project/page.js
+++ src/app/project/page.js
@@ -5,9 +5,11 @@ const Page = () => {
         {
             name: "aram.tools",
             tags: ["React", "Node.js", "Docker"],
-            link: "https://github.com/Hintful/aram.tools"
+            link: "https://github.com/Hintful/aram.tools",
+            description: "A full-stack web application that analyzes and visualizes users' performance for a popular online game, League of Legends."
         }
     ]
+
     return ( 
         <div className="flex-col mt-36 text-white space-y-12">
             <span className="text-white text-2xl">Projects</span>
